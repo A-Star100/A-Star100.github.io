3c649339d95b2f7c5324ad43fe1df5dce1dd0200
Anirudh Sevugan <sonicforces207@gmail.com>
2024-08-27 08:52:55 -0500

    Update msclarity.js

--- msclarity.js
+++ msclarity.js
@@ -1,2 +1,2 @@
-// No Microsoft Clarity tracking exists. If you want the source code with tracking, look at the "with-ms-clarity-tracking" branch. Turns out a cookie consent banner is harder than I thought. I'll use Microsoft Clarity for more business-oriented stuff, why use it for my personal site? (at least, in the published branch) :)
+// No Microsoft Clarity tracking exists (not in this branch). If you want the source code with tracking, look at the "with-ms-clarity-tracking" branch. Turns out a cookie consent banner is harder than I thought. I'll use Microsoft Clarity for more business-oriented stuff, why use it for my personal site? (at least, in the published branch) :)
 
